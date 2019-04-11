ccc43db3b4e061755da0a05065ce4067c307d616
aclouditation <25907312+AClouDitation@users.noreply.github.com>
2019-04-11 12:53:06 -0400

    removed uncessory file, modified build.sh, update OpenCV requirement

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-Qt_PATH=$HOME/Qt/5.12.1/clang_64
-rm -r build
-mkdir build && cd build
+Qt_PATH=$HOME/Qt/5.13.0/gcc_64
+
+mkdir -p build && cd build
 
 cmake -DCMAKE_PREFIX_PATH=$Qt_PATH ..
 make -j4
